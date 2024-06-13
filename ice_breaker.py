643e87d58646291c939f823b4b74257c7840847b
MengskNguyen <107334143+MengskNguyen@users.noreply.github.com>
2024-06-13 19:31:09 +0700

    Add Output Parser

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,36 +1,38 @@
 from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain.chains import LLMChain
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
+from output_parsers import summary_parser
 from third_parties.linkedin import scrape_linkedin_profile
-
-information = """
-Makima (マキマ?) is the main antagonist of the Public Safety Saga. She was the Chief Cabinet Secretary's Personal Devil Hunter who took Denji in as her human pet.
-
-She is later revealed to be the Control Devil (支し配はいの悪あく魔ま Shihai no Akuma?) who embodies the fear of control or conquest and a member of the Four Horsemen at the time. Following her death, she was reincarnated as Nayuta.
-"""
+from third_parties.twitter import scrape_user_tweets
 
 
 def ice_break_with(name: str) -> str:
     linkedin_username = linkedin_lookup_agent(name=name)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
 
+    twitter_username = twitter_lookup_agent(name=name)
+    tweets = scrape_user_tweets(username=twitter_username, mock=True)
+
     summary_template = """"
-        given the information {information} about a person from I want you to create:
+        given the information {information}, and twitter posts {twitter_posts} about a person from I want you to create:
         1. a short summary
         2. two interesting facts about them
+        
+        \n{format_instructions}
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=['information'],
-        template=summary_template
+        input_variables=['information', 'twitter_posts'],
+        template=summary_template,
+        partial_variables={'format_instructions': summary_parser.get_format_instructions()}
     )
     llm = ChatOpenAI(temperature=0, model_name="gpt-4")
-    chain = summary_prompt_template | llm
-    res = chain.invoke(input={'information': linkedin_data})
-    print(res.content)
+    chain = summary_prompt_template | llm | summary_parser
+    res = chain.invoke(input={'information': linkedin_data, 'twitter_posts': tweets})
+    print(res)
 
 
 if __name__ == "__main__":
